6a5ce0d5adaa7876120b803afd515b9719994428
Erik <erikhau3010@gmail.com>
2022-10-05 09:45:58 +0700

    feat: add welcome page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Game from "./Game.js";
 
-const canvas = document.querySelector('#canvas');
+const canvas = document.querySelector("#canvas");
 
-const game = new Game({canvas});
+const game = new Game({ canvas });
 game.init();
+
+// const username = document.querySelector("#username");
+// const level = document.querySelector("#level");
+
+// const startBtn = document.querySelector("#start-btn");
+// const instructionBtn = document.querySelector("#instruction-btn");
+
+// const startGameHandler = () => {};
+
+// startBtn.addEventListener("click", startGameHandler);
